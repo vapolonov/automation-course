4d59382f1fe608aa3680b994c3729c8e677fa4fd
AlgosStile <oleg.todor@mail.ru>
2025-03-18 21:29:44 +0300

    test

---
+++ src/test/java/auto/ExampleTest.java
@@ -0,0 +1,12 @@
+package auto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExampleTest {
+    @Test
+    void dummyTest() {
+        assertTrue(true);
+    }
+}
--- src/test/java/courseplayw/DynamicContentTest.java
+++ src/test/java/courseplayw/DynamicContentTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import java.nio.file.Paths;
 
-class DynamicContentTest {
+public class DynamicContentTest {
 
     Playwright playwright;
     Browser browser;
@@ -33,6 +33,39 @@ class DynamicContentTest {
 
     @Test
     void testDynamicLoading() {
+        try {
+            page.navigate("https://the-internet.herokuapp.com/dynamic_loading/1");
 
+            Locator startButton = page.locator("button:has-text('Start')");
+            startButton.waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
+            startButton.click();
+
+            Locator helloWorldText = page.locator("#finish >> text=Hello World!");
+            helloWorldText.waitFor(new Locator.WaitForOptions().setTimeout(45000));
+
+            Locator seleniumLink = page.locator("text=Elemental Selenium");
+            seleniumLink.waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
+            Assertions.assertTrue(seleniumLink.isVisible(), "Ссылка не отображается");
+
+            Page newPage = context.waitForPage(() -> {
+                seleniumLink.click();
+            });
+
+            newPage.waitForLoadState(LoadState.LOAD);
+
+            Assertions.assertTrue(
+                    newPage.url().startsWith("https://elementalselenium.com/"), "Фактический URL: " + newPage.url()
+            );
+            Assertions.assertTrue(newPage.isVisible("h1 >> text='Elemental Selenium'"), "Заголовок не найден"
+            );
+
+            newPage.close();
+
+        } catch (Exception e) {
+
+            page.screenshot(new Page.ScreenshotOptions()
+                    .setPath(Paths.get("dynamic-loading-error.png")));
+            throw e;
+        }
     }
 }
