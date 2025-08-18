b75a7e0df42fe04a1a986b37544b1184c1847314
vasvap <vasvap@gmail.com>
2025-08-18 11:17:41 +0300

    headless=true

--- src/test/java/practice/StatusCodeInterceptionTest.java
+++ src/test/java/practice/StatusCodeInterceptionTest.java
@@ -20,7 +20,7 @@ public class StatusCodeInterceptionTest {
   @BeforeEach
   void setUp() {
     playwright = Playwright.create();
-    browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+    browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
     context = browser.newContext();
     page = context.newPage();
 
