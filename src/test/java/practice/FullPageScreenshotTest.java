f20155007590ea7d68139519c8288bbe3fd15d00
vasvap <vasvap@gmail.com>
2025-08-21 21:39:13 +0300

    full page

--- src/test/java/practice/FullPageScreenshotTest.java
+++ src/test/java/practice/FullPageScreenshotTest.java
@@ -17,18 +17,7 @@ public class FullPageScreenshotTest extends BaseTest {
   public void testHomePageVisual() throws IOException {
     page.navigate("https://the-internet.herokuapp.com");
     Path actual = Paths.get("actual.jpg");
-    page.screenshot(new Page.ScreenshotOptions().setPath(actual));
-
-//    Path expectedPath = Paths.get("expected.png");
-//
-//    ! Если эталонного файла нет, создаем его из текущего скриншота
-//    if (!Files.exists(expectedPath)) {
-//      Files.copy(actualPath, expectedPath);
-//      System.out.println("Эталонный скриншот создан. Проверьте его корректность и перезапустите тест.");
-//      return; // Прерываем тест при первом запуске
-//    }
-//
-//     long mismatchPosition = Files.mismatch(actualPath, expectedPath);
+    page.screenshot(new Page.ScreenshotOptions().setPath(actual).setFullPage(true));
 
     long mismatch = Files.mismatch(actual, Paths.get("expected.jpg"));
 
