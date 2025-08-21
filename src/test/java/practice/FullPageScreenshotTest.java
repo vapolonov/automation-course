843fd51a58b0795fc188e357677b0068ad9decb6
vasvap <vasvap@gmail.com>
2025-08-21 21:22:26 +0300

    добавлена проверка различий

--- src/test/java/practice/FullPageScreenshotTest.java
+++ src/test/java/practice/FullPageScreenshotTest.java
@@ -19,7 +19,27 @@ public class FullPageScreenshotTest extends BaseTest {
     Path actual = Paths.get("actual.jpg");
     page.screenshot(new Page.ScreenshotOptions().setPath(actual));
 
+//    Path expectedPath = Paths.get("expected.png");
+//
+//    ! Если эталонного файла нет, создаем его из текущего скриншота
+//    if (!Files.exists(expectedPath)) {
+//      Files.copy(actualPath, expectedPath);
+//      System.out.println("Эталонный скриншот создан. Проверьте его корректность и перезапустите тест.");
+//      return; // Прерываем тест при первом запуске
+//    }
+//
+//     long mismatchPosition = Files.mismatch(actualPath, expectedPath);
+
     long mismatch = Files.mismatch(actual, Paths.get("expected.jpg"));
+
+    //! Если найдены различия, сохраняем diff-файл
+    if (mismatch != -1) {
+      Path diffPath = Paths.get("diff.png");
+      Files.copy(actual, diffPath);
+      System.out.println("Обнаружены визуальные различия!");
+      System.out.println("Diff-файл сохранен: " + diffPath.toAbsolutePath());
+    }
+
     assertThat(mismatch).isEqualTo(-1);
   }
 }
